19f147b4b398222efc127f1234caef848e2ebbc9
cho <you-smile@nate.com>
2016-11-29 18:53:21 +0900

    1

---
+++ app/src/main/java/com/bit2016/splashexample/SplashActivity.java
@@ -0,0 +1,22 @@
+package com.bit2016.splashexample;
+
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        // 지연
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // 초기화 작업
+                finish();
+            }
+        }, 1000);
+    }
+}
